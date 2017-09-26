be7cff3d27959122a8a65371fe4f741107495034
pguillie <pguillie@student.42.fr>
2017-09-26 14:41:19 +0200

    lol

--- src/restore.c
+++ src/restore.c
@@ -4,14 +4,14 @@ int		restore(struct termios backup, t_tc tc)
 {
 	int	i;
 
-	i = 0;
 	tputs(tc.cd, 0, termput);
 	tputs(tc.ve, 0, termput);
 	if (tcsetattr(0, TCSANOW, &backup) < 0)
 		return (-1);
 	if (g_sig == SIGTSTP || g_sig == SIGQUIT)
 	{
-		while (i < 100000)
+		i = 0;
+		while (i < 10000000)
 			i++;
 		signal(SIGTSTP, SIG_DFL);
 		signal(SIGQUIT, SIG_DFL);
